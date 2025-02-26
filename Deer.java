7d5a30ad6e46d1242277364bfe27b8342827e122
UlvisTurkers <ediz.flash@gmail.com>
2025-02-26 16:08:50 +0000

    added deer and mice class

---
+++ Deer.java
@@ -0,0 +1,135 @@
+import java.util.List;
+import java.util.Random;
+import javafx.scene.paint.Color;
+
+/**
+ * A simple model of a deer.
+ * Deer are prey animals that age, move, breed, and eventually die.
+ * They breed more slowly compared to rabbits and have a low food value.
+ *
+ * Deer characteristics:
+ * - Breeding age: 10
+ * - Maximum age: 80
+ * - Breeding probability: 0.10
+ * - Maximum litter size: 2
+ * - Food value: 18 (derived from Rabbit.getRabbitFoodValue())
+ *
+ * This class follows a similar structure to the Rabbit class.
+ */
+public class Deer extends Animal {
+
+    // Constants that define deer behavior and properties.
+    private static final int BREEDING_AGE = 10;
+    private static final int MAX_AGE = 80;
+    private static final double BREEDING_PROBABILITY = 0.10;
+    private static final int MAX_LITTER_SIZE = 2;
+    private static final int FOOD_VALUE = 18;
+    
+    // Random number generator for controlling random events like breeding.
+    private static final Random rand = Randomizer.getRandom();
+    
+    // Instance variable to track the age of the deer.
+    private int age;
+
+    /**
+     * Create a new deer.
+     * If randomAge is true, the deer is assigned a random age (up to its maximum age).
+     * Otherwise, it starts as a newborn.
+     *
+     * @param randomAge If true, initialize the deer with a random age.
+     * @param field The field where the deer exists.
+     * @param location The initial location of the deer within the field.
+     * @param col The color representing the deer.
+     */
+    public Deer(boolean randomAge, Field field, Location location, Color col) {
+        super(field, location, col);
+        age = 0;
+        if(randomAge) {
+            age = rand.nextInt(MAX_AGE);
+        }
+    }
+    
+    /**
+     * Define the behavior of the deer during each simulation step.
+     * The deer ages and may breed.
+     * Then it attempts to move into a free adjacent location.
+     * If no such location is available (overcrowding), the deer dies.
+     *
+     * @param newDeer A list to store any newly born deer.
+     */
+    public void act(List<Animal> newDeer) {
+        incrementAge();
+        if(isAlive()) {
+            giveBirth(newDeer);
+            // Try to move into a free adjacent location.
+            Location newLocation = getField().getFreeAdjacentLocation(getLocation());
+            if(newLocation != null) {
+                setLocation(newLocation);
+            } else {
+                // Death occurs if the deer cannot move due to overcrowding.
+                setDead();
+            }
+        }
+    }
+    
+    /**
+     * Increase the deer's age. If the deer exceeds its maximum age, it dies.
+     */
+    private void incrementAge() {
+        age++;
+        if(age > MAX_AGE) {
+            setDead();
+        }
+    }
+    
+    /**
+     * Allow the deer to give birth to new deer.
+     * The number of births is determined by the deer's breeding probability and litter size.
+     *
+     * @param newDeer A list to add any newly born deer.
+     */
+    private void giveBirth(List<Animal> newDeer) {
+        Field field = getField();
+        List<Location> free = field.getFreeAdjacentLocations(getLocation());
+        int births = breed();
+        for (int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+            Deer young = new Deer(false, field, loc, getColor());
+            newDeer.add(young);
+        }
+    }
+    
+    /**
+     * Determine the number of births based on breeding conditions.
+     * A deer will produce a number of offspring if it has reached breeding age
+     * and a random chance (based on BREEDING_PROBABILITY) succeeds.
+     *
+     * @return The number of new deer born (could be zero).
+     */
+    private int breed() {
+        int births = 0;
+        if (canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
+            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        }
+        return births;
+    }
+    
+    /**
+     * Check whether the deer is old enough to breed.
+     *
+     * @return True if the deer's age is at least the breeding age.
+     */
+    private boolean canBreed() {
+        return age >= BREEDING_AGE;
+    }
+    
+    /**
+     * Get the food value of the deer.
+     * This value is used by predators when consuming a deer.
+     *
+     * @return The food value of a deer (18).
+     */
+    public int getFoodValue() {
+        return FOOD_VALUE;
+    }
+}
---
+++ Mice.java
@@ -0,0 +1,137 @@
+import java.util.List;
+import java.util.Random;
+import javafx.scene.paint.Color; 
+
+
+/**
+ * A simple model of a mice.
+ * Mice age, move, breed, and die. They are a type of prey that
+ * breed quickly, have a low food value, and live for a short period.
+ * 
+ * Breeding details for mice:
+ * - Breeding Age: 3 steps
+ * - Maximum Age: 20 steps
+ * - Breeding Probability: 0.15 (15% chance of breeding when conditions are met)
+ * - Maximum Litter Size: 9 offspring per breeding event
+ * - Food Value: 4 units
+ * 
+ * This class is used as part of the predator/prey simulation.
+ */
+
+public class Mice extends Animal {
+
+    // Mice-specific constants.
+    private static final int BREEDING_AGE = 3;
+    private static final int MAX_AGE = 20;
+    private static final double BREEDING_PROBABILITY = 0.15;
+    private static final int MAX_LITTER_SIZE = 9;
+    private static final int FOOD_VALUE = 4;
+    
+    // Random number generator for breeding and age initialization.
+    private static final Random rand = Randomizer.getRandom();
+    
+    // The current age of the mice.
+    private int age;
+
+    /**
+     * Create a new mice. A mice may be created as a newborn or with a random age.
+     *
+     * @param randomAge If true, the mice will have a random age (useful for populating an initial field).
+     * @param field The field currently occupied.
+     * @param location The location within the field.
+     * @param col The color to represent this mice.
+     */
+    public Mice(boolean randomAge, Field field, Location location, Color col) {
+        super(field, location, col);
+        age = 0;
+        
+        // If randomAge is true, initialize with a random age up to the maximum age.
+        if(randomAge) {
+            age = rand.nextInt(MAX_AGE);
+        }
+    }
+    
+    /**
+     * Define the behavior of the mice for each simulation step.
+     * The mice ages, attempts to breed, and moves to a new location if possible.
+     *
+     * @param newMice A list to collect newly born mice.
+     */
+    public void act(List<Animal> newMice) {
+        // Increase the age and check for death due to old age.
+        incrementAge();
+        if(isAlive()) {
+            // Attempt to give birth to new mice.
+            giveBirth(newMice);            
+            // Try to move into a free adjacent location.
+            Location newLocation = getField().getFreeAdjacentLocation(getLocation());
+            if(newLocation != null) {
+                setLocation(newLocation);
+            }
+            else {
+                // If no free location is available, the mice dies due to overcrowding.
+                setDead();
+            }
+        }
+    }
+
+    /**
+     * Increment the age of the mice.
+     * If the age exceeds the maximum allowed age, the mice dies.
+     */
+    private void incrementAge() {
+        age++;
+        if(age > MAX_AGE) {
+            setDead();
+        }
+    }
+    
+    /**
+     * Check if the mice is ready to breed and produce offspring.
+     * New offspring are placed in adjacent free locations.
+     *
+     * @param newMice A list to collect newly born mice.
+     */
+    private void giveBirth(List<Animal> newMice) {
+        Field field = getField();
+        List<Location> free = field.getFreeAdjacentLocations(getLocation());
+        int births = breed();
+        for(int b = 0; b < births && free.size() > 0; b++) {
+            Location loc = free.remove(0);
+            Mice young = new Mice(false, field, loc, getColor());
+            newMice.add(young);
+        }
+    }
+        
+    /**
+     * Determine the number of births based on the mice's breeding conditions.
+     *
+     * @return The number of new mice born, or zero if the mice cannot breed.
+     */
+    private int breed() {
+        int births = 0;
+        if(canBreed() && rand.nextDouble() <= BREEDING_PROBABILITY) {
+            births = rand.nextInt(MAX_LITTER_SIZE) + 1;
+        }
+        return births;
+    }
+
+    /**
+     * Check whether the mice is of breeding age.
+     *
+     * @return true if the mice is old enough to breed, false otherwise.
+     */
+    private boolean canBreed() {
+        return age >= BREEDING_AGE;
+    }
+    
+    /**
+     * Get the food value of the mice.
+     * This value is used by predators to determine the nutritional benefit.
+     *
+     * @return the food value of the mice.
+     */
+    public int getFoodValue() {
+        return FOOD_VALUE;
+    }
+}

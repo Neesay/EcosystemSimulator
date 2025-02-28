cf6ac8a544093f1b7656ebd416a1ad5277b0711c
Yaseen Alam <58012269+Neesay@users.noreply.github.com>
2025-02-28 17:25:31 +0000

    remove varibales

--- Wolf.java
+++ Wolf.java
@@ -18,13 +18,6 @@ import javafx.scene.paint.Color;
  */
 public class Wolf extends Animal {
     
-    private static int BREEDING_AGE;
-    private static int MAX_AGE;
-    private static double BREEDING_PROBABILITY;
-    private static double DISEASE_PROBABILITY;
-    private static int MAX_LITTER_SIZE;
-    private static double METABOLISM;
-    
     private static final int MAX_FOOD_VALUE = 15;
     private static final Random rand = Randomizer.getRandom();
     

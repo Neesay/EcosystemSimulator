543adf47c29b4bc9977a7bb64ecd95f671dae35a
UlvisTurkers <ediz.flash@gmail.com>
2025-03-05 12:56:58 +0000

    increased breeding prob for mouse

--- Mouse.java
+++ Mouse.java
@@ -10,8 +10,8 @@ public class Mouse extends Prey {
         // Initialize gene parameters for Mice.
         gene.BREEDING_AGE = rand.nextInt(2, 5);
         gene.MAX_AGE = rand.nextInt(4, 15);
-        gene.BREEDING_PROBABILITY = rand.nextDouble(0.12, 0.18);
-        gene.DISEASE_PROBABILITY = gene.BREEDING_PROBABILITY - 0.01;
+        gene.BREEDING_PROBABILITY = rand.nextDouble(0.8, 0.9);
+        gene.DISEASE_PROBABILITY = rand.nextDouble(0.05, 0.1);
         gene.MAX_LITTER_SIZE = rand.nextInt(7, 11);
         gene.MAX_FOOD_VALUE = rand.nextInt(9, 12);
         gene.METABOLISM = rand.nextDouble(0.25, 1.0);
